132cab7764c4cd9ac938aef5c82597d2494c85e3
HandcraftedBits <opensource@handcraftedbits.com>
2016-09-21 21:24:47 -0400

    Update Nginx and headers-more versions, fix renewal script.

--- data/opt/container/script/renew.sh
+++ data/opt/container/script/renew.sh
@@ -3,7 +3,7 @@
 # Renews Let's Encrypt certificates.
 # Adapted from https://letsecure.me/secure-web-deployment-with-lets-encrypt-and-nginx/
 
-if [ ! certbot renew > /var/log/letsencrypt/renew.log 2>&1 ]
+if [ ! certbot renew --webroot --webroot-path /var/www/letsencrypt-well-known > /var/log/letsencrypt/renew.log 2>&1 ]
 then
      echo "[error] Let's Encrypt automated certificate renewal failed:"
      cat /var/log/letsencrypt/renew.log
